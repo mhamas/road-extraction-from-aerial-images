5c29bd70f19c494660af5b49afa37e4b7407bcca
taivop <taivo.pungas@gmail.com>
2016-06-20 11:50:54 +0300

    revert to matplotlib for reading images

--- src/model_taivo_test.py
+++ src/model_taivo_test.py
@@ -1,8 +1,7 @@
 import os
 import sys
 import urllib
-#import matplotlib.image as mpimg
-from scipy import misc
+import matplotlib.image as mpimg
 from PIL import Image
 
 import csv
@@ -397,10 +396,10 @@ def main(argv=None):  # pylint: disable=unused-argument
             return res_img
 
         # Read images from disk
-        img = misc.imread(input_path)
+        img = mpimg.imread(input_path)
         img_truth = None
         if truth_input_path != None:
-            img_truth = misc.imread(truth_input_path)
+            img_truth = mpimg.imread(truth_input_path)
 
         # Get prediction
         stride = 4  # TODO const.IMG_PATCH_SIZE
