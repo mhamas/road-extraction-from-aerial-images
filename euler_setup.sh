c8f925d6aba6dd7dee7169ebfd7ba4ffe6624d40
taivop <taivo.pungas@gmail.com>
2016-06-21 22:00:24 +0300

    update setup script. need to run manually still

--- euler_setup.sh
+++ euler_setup.sh
@@ -2,6 +2,9 @@
 
 
 module load eth_proxy gcc/4.9.2 python/3.3.3 openblas/0.2.13_par
+
+source ~/.venv/bin/activate
+
 module load zlib
 
 export BLAS=/cluster/apps/openblas/0.2.13_par/x86_64/gcc_4.9.2/lib/libopenblas.so
@@ -10,5 +13,3 @@ export ATLAS=/cluster/apps/openblas/0.2.13_par/x86_64/gcc_4.9.2/lib/libopenblas.
 
 export C_INCLUDE_PATH="$C_INCLUDE_PATH:/cluster/apps/python/3.3.3/x86_64/include/python3.3m/"
 export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/cluster/apps/python/3.3.3/x86_64/include/python3.3m/"
-
-source ~/.venv/bin/activate
