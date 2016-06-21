85ab6011d5cae90d2c910c5fb37c6ba1b0cb1891
taivop <taivo.pungas@gmail.com>
2016-06-21 21:58:05 +0300

    split sh

--- euler_run_64G_memory.sh
+++ euler_run_64G_memory.sh
@@ -7,22 +7,8 @@
 # -oo specifies output file
 
 
-module load eth_proxy gcc/4.9.2 python/3.3.3 openblas/0.2.13_par
-module load zlib
-
-export BLAS=/cluster/apps/openblas/0.2.13_par/x86_64/gcc_4.9.2/lib/libopenblas.so
-export LAPACK=/cluster/apps/openblas/0.2.13_par/x86_64/gcc_4.9.2/lib/libopenblas.so
-export ATLAS=/cluster/apps/openblas/0.2.13_par/x86_64/gcc_4.9.2/lib/libopenblas.so
-
-export C_INCLUDE_PATH="$C_INCLUDE_PATH:/cluster/apps/python/3.3.3/x86_64/include/python3.3m/"
-export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/cluster/apps/python/3.3.3/x86_64/include/python3.3m/"
-
-source ~/.venv/bin/activate
-
 mkdir logs
 cd src
 bsub -n 8 -R "rusage[mem=8192]" -We 3:55 -oo "../logs/run_%J.out" \
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/ext/lib" $HOME/ext/lib/ld-2.23.so $HOME/.venv/bin/python3 model_taivo_test.py
 cd ..
-
-deactivate # leave venv
