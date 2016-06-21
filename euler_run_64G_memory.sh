b691f83708ae3e2f242cf01037f8bdc835694335
taivop <taivo.pungas@gmail.com>
2016-06-21 21:43:10 +0300

    environment setup also to euler script

--- euler_run_64G_memory.sh
+++ euler_run_64G_memory.sh
@@ -6,6 +6,19 @@
 # -W walltime
 # -oo specifies output file
 
+
+module load eth_proxy gcc/4.9.2 python/3.3.3 openblas/0.2.13_par
+module load zlib
+
+export BLAS=/cluster/apps/openblas/0.2.13_par/x86_64/gcc_4.9.2/lib/libopenblas.so
+export LAPACK=/cluster/apps/openblas/0.2.13_par/x86_64/gcc_4.9.2/lib/libopenblas.so
+export ATLAS=/cluster/apps/openblas/0.2.13_par/x86_64/gcc_4.9.2/lib/libopenblas.so
+
+export C_INCLUDE_PATH="$C_INCLUDE_PATH:/cluster/apps/python/3.3.3/x86_64/include/python3.3m/"
+export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/cluster/apps/python/3.3.3/x86_64/include/python3.3m/"
+
+source ~/.venv/bin/activate
+
 mkdir logs
 cd src
 bsub -n 8 -R "rusage[mem=8192]" -We 3:55 -oo "../logs/run_%J.out" \
