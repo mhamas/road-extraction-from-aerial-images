feeb7e2fe2cc231e72d56bbab6b5a59799c7e61f
taivop <taivo.pungas@gmail.com>
2016-06-28 14:50:42 +0300

    update run.py: include upsampling

--- src/run.py
+++ src/run.py
@@ -9,11 +9,21 @@ are not found cached on the disk, this script automatically trains them
 
 
 """
+import glob
 import postprocessing as pp
 import model_taivo_test as cnn
+import upsampling as upsampling
 
 # Train CNN
 cnn.main()
+
+# Upsample predictions for both training and test set
+UPSAMPLE = True
+if UPSAMPLE:
+    training_filenames = glob.glob("../results/CNN_Output/training/*/*.png")
+    test_filenames = glob.glob("../results/CNN_Output/test/*/*.png")
+    upsampling.upsample_training(training_filenames)
+    upsampling.upsample_test(test_filenames)
 
 # Apply post processing to CNN output
 pp.generate_output()
