5064af77678590b5d825f5f85b127c7a4a35c7b2
taivop <taivo.pungas@gmail.com>
2016-06-28 15:20:50 +0300

    remove the experiments (in matlab) that we wont use

--- src/graphcut/GraphCut/BK_Minimize.m
+++
@@ -1,9 +0,0 @@
-function Energy = BK_Minimize(Handle)
-% BK_Minimize   Compute optimal labeling via graph cut
-%    Returns the energy of the computed labeling.
-%    The labeling itself can be retrieved via GCO_GetLabeling.
-%
-
-BK_LoadLib();
-Energy = bk_matlab('bk_minimize',Handle);
-end
