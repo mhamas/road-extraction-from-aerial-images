06a3d85860185993fb69ee52447a512e18df9f25
taivop <taivo.pungas@gmail.com>
2016-06-28 12:42:19 +0300

    setup sh script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Create output directory structure
+mkdir -p results/CNN_Output/test/raw
+mkdir -p results/CNN_Output/test/high_res_raw
+mkdir -p results/CNN_Output/training/raw
+mkdir -p results/CNN_Output/training/high_res_raw
