5671ad5a7bb7748d22b4ca90df06fe202d2cefde
taivop <taivo.pungas@gmail.com>
2016-06-29 15:34:22 +0300

    baseline now runs without errors. havent checked output

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ mkdir -p results/CNN_Output/test/high_res_raw
 mkdir -p results/CNN_Output/training/raw
 mkdir -p results/CNN_Output/training/high_res_raw
 
+mkdir -p results/CNN_Output_Baseline/test/raw
+mkdir -p results/CNN_Output_Baseline/training/raw
+
 # Create tmp directories to hold TensorFlow results
 mkdir -p tmp
 mkdir -p src/baseline/tmp
