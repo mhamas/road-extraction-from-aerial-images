af24411d875cb82b6133157b8ef44259b6e8a015
taivop <taivo.pungas@gmail.com>
2016-06-30 00:07:38 +0300

    euler to dropbox script is now simpler and has baseline

--- sh/euler_upload_to_dropbox.sh
+++ sh/euler_upload_to_dropbox.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
 # Uses https://github.com/andreafabrizi/Dropbox-Uploader
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/test
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/test/raw
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/test/high_res_raw
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/training
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/training/raw
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/training/high_res_raw
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /predictions_training
-~/Dropbox-Uploader/dropbox_uploader.sh mkdir /predictions_test
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/test
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/test/raw
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/test/high_res_raw
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/training
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/training/raw
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /CNN_Output/training/high_res_raw
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /predictions_training
+#~/Dropbox-Uploader/dropbox_uploader.sh mkdir /predictions_test
+#
+#~/Dropbox-Uploader/dropbox_uploader.sh upload ~/eth-cil-project/results/CNN_Output/test/raw/*.png /CNN_Output/test/raw
+#~/Dropbox-Uploader/dropbox_uploader.sh upload ~/eth-cil-project/results/CNN_Output/test/high_res_raw/*.png /CNN_Output/test/high_res_raw
+#
+#~/Dropbox-Uploader/dropbox_uploader.sh upload ~/eth-cil-project/results/CNN_Output/training/raw/*.png /CNN_Output/training/raw
+#~/Dropbox-Uploader/dropbox_uploader.sh upload ~/eth-cil-project/results/CNN_Output/training/high_res_raw/*.png /CNN_Output/training/high_res_raw
 
-~/Dropbox-Uploader/dropbox_uploader.sh upload ~/eth-cil-project/results/CNN_Output/test/raw/*.png /CNN_Output/test/raw
-~/Dropbox-Uploader/dropbox_uploader.sh upload ~/eth-cil-project/results/CNN_Output/test/high_res_raw/*.png /CNN_Output/test/high_res_raw
 
-~/Dropbox-Uploader/dropbox_uploader.sh upload ~/eth-cil-project/results/CNN_Output/training/raw/*.png /CNN_Output/training/raw
-~/Dropbox-Uploader/dropbox_uploader.sh upload ~/eth-cil-project/results/CNN_Output/training/high_res_raw/*.png /CNN_Output/training/high_res_raw
+~/Dropbox-Uploader/dropbox_uploader.sh copy ~/eth-cil-project/results/CNN_Output /
+~/Dropbox-Uploader/dropbox_uploader.sh copy ~/eth-cil-project/results/CNN_Output_Baseline /
