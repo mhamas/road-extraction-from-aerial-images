47ceb7ef61d1c826bffa7430e49d5ec79a235f54
taivop <taivo.pungas@gmail.com>
2016-06-30 11:57:58 +0300

    fix csv file path

--- src/baseline/model_baseline.py
+++ src/baseline/model_baseline.py
@@ -580,7 +580,7 @@ def main(argv=None):  # pylint: disable=unused-argument
 
             return img_prediction
 
-        with open('baseline_submission.csv', 'w') as csvfile:
+        with open('../results/baseline_submission.csv', 'w') as csvfile:
             writer = csv.writer(csvfile, delimiter=',')
             writer.writerow(['id', 'prediction'])
             for i in range(1, TEST_SIZE + 1):
